6a036ffe7192c06a8f4c3c56a93a43642be06b2f
mreugenej7 <eumadeinchina@gmail.com>
2018-11-06 09:46:15 +0000

    codificado g_sat

--- lib/nutrientesEugenio.rb
+++ lib/nutrientesEugenio.rb
@@ -3,11 +3,12 @@ require "nutrientesEugenio/version"
 
 
 class Alimento 
-    attr_reader :nombre, :grasas
+    attr_reader :nombre, :grasas, :g_sat
       
-    def initialize(nombre, grasas)
+    def initialize(nombre, grasas, g_sat)
         @nombre = nombre
         @grasas = grasas
+        @g_sat = g_sat
     end
 end
 
