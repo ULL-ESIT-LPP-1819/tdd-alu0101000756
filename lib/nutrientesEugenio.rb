a53608170685aacc0fefa9908e680c25ab59e748
mreugenej7 <eumadeinchina@gmail.com>
2018-11-06 09:48:54 +0000

    codificado hidratos de carbono

--- lib/nutrientesEugenio.rb
+++ lib/nutrientesEugenio.rb
@@ -3,12 +3,13 @@ require "nutrientesEugenio/version"
 
 
 class Alimento 
-    attr_reader :nombre, :grasas, :g_sat
+    attr_reader :nombre, :grasas, :g_sat, :h_car
       
-    def initialize(nombre, grasas, g_sat)
+    def initialize(nombre, grasas, g_sat, h_car)
         @nombre = nombre
         @grasas = grasas
         @g_sat = g_sat
+        @h_car = h_car
     end
 end
 
