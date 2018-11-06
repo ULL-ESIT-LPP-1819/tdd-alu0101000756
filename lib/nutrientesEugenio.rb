38c8a778a975aebf40a42ca3acb3da25a2637921
mreugenej7 <eumadeinchina@gmail.com>
2018-11-06 09:55:02 +0000

    codificado sal

--- lib/nutrientesEugenio.rb
+++ lib/nutrientesEugenio.rb
@@ -3,15 +3,16 @@ require "nutrientesEugenio/version"
 
 
 class Alimento 
-    attr_reader :nombre, :grasas, :g_sat, :h_car, :azucar, :proteina
+    attr_reader :nombre, :grasas, :g_sat, :h_car, :azucar, :proteina, :sal
       
-    def initialize(nombre, grasas, g_sat, h_car, azucar, proteina)
+    def initialize(nombre, grasas, g_sat, h_car, azucar, proteina, sal)
         @nombre = nombre
         @grasas = grasas
         @g_sat = g_sat
         @h_car = h_car
         @azucar = azucar
         @proteina = proteina
+        @sal = sal
     end
 end
 
