9d22dac0e27b06920736e8a56e017798248eea45
mreugenej7 <eumadeinchina@gmail.com>
2018-11-06 09:50:52 +0000

    codificado azucar

--- lib/nutrientesEugenio.rb
+++ lib/nutrientesEugenio.rb
@@ -3,13 +3,14 @@ require "nutrientesEugenio/version"
 
 
 class Alimento 
-    attr_reader :nombre, :grasas, :g_sat, :h_car
+    attr_reader :nombre, :grasas, :g_sat, :h_car, :azucar
       
-    def initialize(nombre, grasas, g_sat, h_car)
+    def initialize(nombre, grasas, g_sat, h_car, azucar)
         @nombre = nombre
         @grasas = grasas
         @g_sat = g_sat
         @h_car = h_car
+        @azucar = azucar
     end
 end
 
