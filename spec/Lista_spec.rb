48df2625e497325eff9360b011bdc7ba678c199a
mreugenej7 <eumadeinchina@gmail.com>
2018-11-12 13:54:27 +0000

    pruebas de los atributos

--- spec/Lista_spec.rb
+++ spec/Lista_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe Lista do
   it "has a version number" do
     expect(Lista::VERSION).not_to be nil
   end
+   context "Nodo" do
+    it "tiene atributos value, next y prev" do
+        expect(@nodo1).to have_attributes(:value => 15, :next => nil, :prev => nil)
+    end
+  end
+  
+  context "Lista" do
+    it "tiene cabeza y cola" do
+        expect(@list1).to have_attributes(:head => nil, :tail => nil)
+    end
+end
 end
