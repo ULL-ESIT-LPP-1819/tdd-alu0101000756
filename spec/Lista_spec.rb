cadbe7a721a21a3f392dadc2d9d59d9c53833244
mreugenej7 <eumadeinchina@gmail.com>
2018-11-12 13:48:22 +0000

    Pruebas de la lista

---
+++ spec/Lista_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Lista do
+  before :each do
+    @nodo1 = Node.new(15, nil, nil)
+    @list1 = Lista.new()
+  end
+  it "has a version number" do
+    expect(Lista::VERSION).not_to be nil
+  end
+end
