fbdc3c4e2da46ec84ad12b4499bb2e1f3a11472e
mreugenej7 <eumadeinchina@gmail.com>
2018-11-12 13:58:38 +0000

    Es enumerable

--- lib/lista.rb
+++ lib/lista.rb
@@ -18,4 +18,22 @@ class Lista
     include Enumerable
     attr_reader :head, :tail
     
+    def push_head(x)
+        if(@head == nil)
+            @head = Node.new(x, nil, nil)
+            @tail = @head
+        else
+            nuevo = Node.new(x, nil, nil)
+            nuevo.next= @head
+            @head.prev = nuevo
+            @head = nuevo
+        end
+    end
+        def each
+        x = @head
+        while(x != nil)
+            yield x.value
+            x = x.next
+        end
+        end
 end
