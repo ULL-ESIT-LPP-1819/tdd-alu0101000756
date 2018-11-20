8ba7b397bb6f21677292f03212cf1f2e4a445cc3
mreugenej7 <eumadeinchina@gmail.com>
2018-11-20 15:28:27 +0000

    prueba categoria

--- spec/individuo_spec.rb
+++ spec/individuo_spec.rb
@@ -2,6 +2,7 @@ require "spec_helper"
 
 RSpec.describe Individuo do
   before :each do
+      @a = []
       @a[0] = Individuo.new("Eugenio", false, true, 73, 1.82, 25, 1)
       @a[1] = Individuo.new("Eugenio", false, true, 73, 1.82, 25, 1)
       @a[2] = Individuo.new("Eugenio", false, true, 73, 1.82, 25, 1)
@@ -16,16 +17,19 @@ RSpec.describe Individuo do
   
     context "Individuo" do
         it "Tiene los atributos necesarios" do
-            expect(@j).to have_attributes(:nombre => "Eugenio", :paciente => false, :t_obesidad => true, :peso => 73, :altura => 1.82, :edad => 25, :sexo => 1)
+            expect(@a[0]).to have_attributes(:nombre => "Eugenio", :paciente => false, :t_obesidad => true, :peso => 73, :altura => 1.82, :edad => 25, :sexo => 1)
         end
         it "Es de la clase correcta" do
-            expect(@j.class).to eq(Individuo)
+            expect(@a[0].class).to eq(Individuo)
         end
         it "Su super clase es la correcta" do
-            expect(@j.class.superclass).to eq(IMC)
+            expect(@a[0].class.superclass).to eq(IMC)
         end
         it "El tipo es el correcto" do
-            expect(@j.is_a?(Individuo)).to eq(true)
+            expect(@a[0].is_a?(Individuo)).to eq(true)
+        end
+        it "categoriza segun imc" do
+            expect(@lst.head.value.cat_peso).to eq("Saludable")
         end
             
     end
