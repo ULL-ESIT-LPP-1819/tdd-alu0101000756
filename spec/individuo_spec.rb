bdc5a8ac5a761a98d37df03ab0400b61114b8a04
mreugenej7 <eumadeinchina@gmail.com>
2018-11-20 14:38:42 +0000

    Prueba atributos

--- spec/individuo_spec.rb
+++ spec/individuo_spec.rb
@@ -2,14 +2,15 @@ require "spec_helper"
 
 RSpec.describe Individuo do
   before :each do
+      @j = Individuo.new("Eugenio", false, true, 73, 1.82, 25, 1)
   end
   it "has a version number" do
     expect(Individuo::VERSION).not_to be nil
   end
   
     context "Individuo" do
-        it "tiene cabeza y cola" do
-            
+        it "Tiene los atributos necesarios" do
+            expect(@j).to have_attributes(:nombre => "Eugenio", :paciente => false, :t_obesidad => true, :peso => 73, :altura => 1.82, :edad => 25, :sexo => 1)
         end
      
     end
